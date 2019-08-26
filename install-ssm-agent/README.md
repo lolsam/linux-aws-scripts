f3d4ee9edefd3f0ceadd1bd2287be6c702684482
sam-it <samit@MyMac.local>
2019-08-25 22:48:49 -0500

    Committing..

---
+++ install-ssm-agent/README.md
@@ -0,0 +1,14 @@
+### Installing AWS SSM Agent
+
+AWS Systems Manager is one of the most useful tools in the AWS ecosystem for managing fleet of instances.  
+
+Imagine if there is a need to install a certain package or execute a shell command on 100s of servers. SSH'ing into each one would take hours. SSM simplifies this task by allowing us to take action on one or hundreds of instances at once.  
+
+However, for AWS to talk to EC2 instances, SSM agent must be installed on the server. With the exception of Amazon Linux AMI, SSM agent is not installed by default on other Linux distros.  
+
+Shell script in this repository checks if SSM Manger is already installed on your system. If it is not, it grabs the latest SSM agent from AWS' website and installs it on your system then starts the service.  
+
+How to call this script:  
+```
+wget -O - https://raw.githubusercontent.com/lolsam/samsetup/master/samsetup.sh | bash -x
+```
---
+++ install-ssm-agent/install-ssm-agent.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SSMLINK=https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-manual-agent-install.html#agent-install-al
+SSMPKG=/tmp/ssm-agent.rpm
+
+checkssm() {
+ echo "*****Checking if SSM agent is already installed*****"
+ if [ ! -z $(which amazon-ssm-agent) ]; then
+  echo ""
+  echo "SSM agent is already installed in `which amazon-ssm-agent`"
+  echo ""
+  sleep 2
+  echo "*****Checking if SSM is running*****"
+  sleep 3
+ fi
+ SSM_RUNNING=`ps -ef | grep ['a']mazon-ssm-agent | wc -l`
+ if [ ${SSM_RUNNING} -gt 0 ]; then
+  echo "SSM agent is already installed and running -- nothing left to do"
+  echo ""
+  ps -ef | grep ['a']mazon-ssm-agent
+  exit 1
+ else
+ echo "SSM agent is installed but not running.. starting it.."
+ fi
+ systemctl restart amazon-ssm-agent
+ RET_VAL=$?
+ if [ ${RET_VAL} -eq 0 ]; then
+  echo "SSM agent successfully started!"
+ exit 1
+  else
+ echo "Could not start SSM agent.. exiting!"
+ fi
+ exit 1
+}
+
+installssm() {
+ if [ ! -f /etc/redhat-release ]; then
+  echo "This is not a RedHat distro, please go to ${SSMLINK} to find the right installation fine"
+ else
+ echo "Installing SSM.."
+ curl ${SSMLINK} -o ${SSMPKG} && rpm -Uvh ${SSMPKG}
+ rm -rf ${SSMPKG}
+ fi
+ RET_VAL=$?
+ if [ ${RET_VAL} -gt 0 ]; then
+  echo "Error in installing SSM agent, exiting!!"
+ fi
+}
+checkssm
+installssm
