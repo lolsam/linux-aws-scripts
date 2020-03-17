f9b4435b4e6589ba95e2ce891d226b81b7816dfc
lolsam <adhikarisamit@gmail.com>
2020-03-17 11:24:46 -0500

    Create install-ssm-agent-ubuntu.sh

---
+++ install-ssm-agent/install-ssm-agent-ubuntu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+{
+SSMLINK=https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+
+checkssm() {
+ echo "*****Checking if SSM agent is already installed*****"
+ if [ ! -z $(which amazon-ssm-agent) ]; then
+  echo ""
+  echo "SSM agent is already installed in `which amazon-ssm-agent`"
+  echo ""
+  exit 1 
+fi
+}
+
+installssm() {
+mkdir /tmp/ssm
+cd /tmp/ssm
+wget ${SSMLINK}
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
+}
+#Calling functions
+checkssm 
+installssm
+
+#Setup logrotate for SSM log files to maintain disk space 
+
+sudo bash -c "cat << EOF > /etc/logrotate.d/amazon
+/var/log/amazon/ssm/errors.log {
+        daily
+        missingok
+        rotate 4
+        compress
+        delaycompress
+        copytruncate
+        minsize 100k
+}
+EOF"
+
+sudo logrotate -f /etc/logrotate.d/amazon
